cf8a9c3385e4072fbba0d2a638d7290e6e76bb3d
constancecchen <constance.chen.3@gmail.com>
2018-01-05 23:20:26 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+![Hipster Fish Logo](./images/logo.png)
+
+# What is this shenaniganry?
+
+Oh hey! This is my (Constance, the person who is narrating) personal boilerplate for starting quick sites like landing pages or other front-ends (becuase there clearly aren't enough frameworks out there on the internet). It's meant to be fairly simple and reusable, but above all individual. But feel free to fork or what have you!
+
+## But why "sea-bass"?
+
+I dunno, it was some god-awful pun I came up with in the middle of the night after eating too much pizza. Here was my thought process:
+
+- My first and last name starts with "C"
+- This is a "base" framework of sorts
+- C-Base is probably already taken
+- Oh wait! I love vintage hipster logos
+
+Thus... sea bass(?)
+
+## Where else has this tomfoolery proliferated?
+
+Huh, let's see, I know have variations of this somewhere!
+
+- [My portfolio site (I would hope so anyway)](https://www.constancechen.me)
+- [Seattle Indies](https://www.seattleindies.org)
+- ...and more to come!
+
+## Usage
+
+- Download or clone
+- `npm install`
+- `gulp`
---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var webserver = require('gulp-webserver');
+
+// Sass compiling
+gulp.task('css', function() {
+  return gulp.src('css/**/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('css/'))
+});
+/* Depending on the project and team size, also consider adding:
+ * https://www.npmjs.com/package/gulp-uncss
+ * https://github.com/milworm/gulp-css-gsub
+ * https://www.npmjs.com/package/gulp-scss-lint
+ */
+
+// JS compiling
+gulp.task('js', function() {
+  return gulp.src('js/**/*.js')
+    .pipe(concat('script.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('js/'))
+});
+
+// Localhost and live reload
+gulp.task('webserver', function() {
+  return gulp.src('./')
+    .pipe(webserver({
+      livereload: true,
+      open: true
+    }));
+});
+
+// Putting it all together
+gulp.task('default', ['css', 'js', 'webserver'], function() {
+  gulp.watch('css/**/*.scss', ['css']);
+  gulp.watch('js/**/*.js', ['js']);
+})
